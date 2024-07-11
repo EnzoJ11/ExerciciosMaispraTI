902f60f456e106daf096efcec96089be08b43ee9
EnzoJ11 <janssgamer0@gmail.com>
2024-07-11 16:50:57 -0300

    exercicioaula35

---
+++ Exercicioaula35/stateHooks/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from './components/counter'
+import MirrorText from './components/mirrorText'
+import ToDoList from './components/toDoList'
+
+function App() {
+  return (
+    <>
+      <Counter />
+      <MirrorText />
+      <ToDoList />
+    </>
+  )
+}
+
+export default App
---
+++ Exercicioaula35/stateHooks/src/components/counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Você clicou {count} vezes</p>
+      <button onClick={() => setCount(count + 1)}>
+        Clique aqui
+      </button>
+    </div>
+  )
+}
+
+export default Counter
